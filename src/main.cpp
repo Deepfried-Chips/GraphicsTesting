93a71f1564a24131fe549a23c86eeb9d7ea7f4e8
Deepfried-Chips <33133709+Deepfried-Chips@users.noreply.github.com>
2023-09-06 14:48:20 +0300

    wawa

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
-#include <iostream>
-#include "glad/gl.h"
-#include "LauncherWindow.h"
-#define GLFW_INCLUDE_NONE
+#include "Widgets/LauncherWindow.h"
 #include "GLFW/glfw3.h"
+
 #include <QApplication>
+#include <QCommonStyle>
+#include <QStyleFactory>
+#include <QMessageBox>
 
-void error_callback(int error, const char* description)
+void shutdownGLFW()
 {
-    fprintf(stderr, "Error: %s\n", description);
-}
-
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    glfwTerminate();
 }
 
 int main(int argc, char *argv[]) {
 
     QApplication a(argc, argv);
 
+    if (!glfwInit())
+    {
+        QMessageBox::critical(nullptr, "Fatal Error", "Failed to initialize 3D video modes");
+        return 1;
+    }
+
+    QCommonStyle *style = (QCommonStyle *)QStyleFactory::create( "fusion" );
+    QApplication::setStyle( style );
+
     LauncherWindow w;
     w.show();
+
+    atexit(shutdownGLFW);
     return QApplication::exec();
 }
